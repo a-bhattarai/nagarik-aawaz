ea32354ba7155849dada2a8aee9ea45d2c8c00dd
Pratistha Subedi <pisubedi17@gmail.com>
2026-07-08 17:12:31 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 const app = express();
 connectDB();
 app.use(express.json());
 app.use(cors());
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/complaints', require('./routes/complaintRoutes'));
-app.use('/api/budgets', require('./routes/budgetRoutes'));   // ← ADD THIS LINE
-app.get('/', (req, res) => {
-  res.send('Nagarik Aawaz API running');
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/budgets", require("./routes/budgetRoutes"));
+app.use("/api/complaints", require("./routes/complaintRoutes"));
+app.get("/", (req, res) => {
+  res.send("Nagarik Aawaz API running");
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
