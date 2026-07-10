e4d72bfe4141c840f0588022cfd2473d2844ba63
Archana Bhattarai <archanabhattarai2006@gmail.com>
2026-07-10 12:16:53 +0545

    server,js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const app = express();
 connectDB();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/budgets", require("./routes/budgetRoutes"));
